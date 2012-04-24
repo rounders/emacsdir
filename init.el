560aa8d01ad8f631749d1de7a63a997618d955c2
Francois Harbec <fharbec@gmail.com>
2012-04-24 13:03:48 -0700

    use bash when running specs instead of zsh

--- init.el
+++ init.el
@@ -44,6 +44,12 @@
 (setq rspec-use-rake-flag nil)
 (define-key rspec-mode-verifible-keymap (kbd "s") 'rspec-verify-single)
 
+(defadvice rspec-compile (around rspec-compile-around)
+  "Use BASH shell for running the specs because of ZSH issues."
+  (let ((shell-file-name "/bin/bash"))
+    ad-do-it))
+(ad-activate 'rspec-compile)
+
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 (require 'peepopen)
 (textmate-mode)
