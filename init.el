8ee4995717089b53ad4e33450164b4879a1c48ce
Francois Harbec <fharbec@gmail.com>
2012-06-12 16:17:59 -0700

    set different frame size depending on screen res

--- init.el
+++ init.el
@@ -87,8 +87,25 @@
                :features rhtml-mode)))
 (el-get 'sync)
 
-(if (window-system)
-  (set-frame-width (selected-frame) 200))
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 200))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
 
 (global-set-key (kbd "C-x C-g") 'magit-status)
 (global-set-key (kbd "M-s-“") 'indent-region)
