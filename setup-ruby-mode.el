f4258f83721cf533a10f75cbb48c636cf94aecad
Francois Harbec <fharbec@gmail.com>
2012-10-23 19:26:19 -0700

    fix indentation of multi-line hash method attributes

--- setup-ruby-mode.el
+++ setup-ruby-mode.el
@@ -5,4 +5,24 @@
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 
+;; http://stackoverflow.com/questions/7404816/emacs-ruby-mode-indenting-wildly-inside-parentheses
+(setq ruby-deep-indent-paren nil)
+
+;; http://stackoverflow.com/questions/7961533/emacs-ruby-method-parameter-indentation
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+
 (provide `setup-ruby-mode)
