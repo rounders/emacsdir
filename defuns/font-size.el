d4d5f90c996b74c895e44f252994eb9c53e662f5
Francois Harbec <fharbec@gmail.com>
2012-10-30 10:53:02 -0700

    add methods for changing font size up/down

---
+++ defuns/font-size.el
@@ -0,0 +1,14 @@
+(defun increase-font-size ()
+  (interactive)
+  (set-face-attribute 'default
+                      nil
+                      :height
+                      (ceiling (* 1.10
+                                  (face-attribute 'default :height)))))
+(defun decrease-font-size ()
+  (interactive)
+  (set-face-attribute 'default
+                      nil
+                      :height
+                      (floor (* 0.9
+                                  (face-attribute 'default :height)))))
