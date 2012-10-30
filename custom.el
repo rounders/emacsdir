1acc7402f771d7dfd19f2dc52cef58b5a3086ac7
Francois Harbec <fharbec@gmail.com>
2012-10-30 10:48:22 -0700

    made some theme changes

--- custom.el
+++ custom.el
@@ -4,8 +4,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(ansi-term-color-vector [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"])
+ '(background-color "#042028")
+ '(background-mode dark)
+ '(cursor-color "#708183")
  '(custom-enabled-themes (quote (pastels-on-dark)))
- '(custom-safe-themes (quote ("30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" default))))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "bbebe9c8b15264adf0eb36bfd1d864b5ad8fdd679ea0d7988c5c6de591e15d12" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" default)))
+ '(fci-rule-color "#383838")
+ '(foreground-color "#708183")
+ '(grep-find-ignored-directories (quote (".git" "log" "tmp")))
+ '(grep-find-ignored-files (quote (".#*" "*.jpg" "*.jpeg" "*.gif" "*.png"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
