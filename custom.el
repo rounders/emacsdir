3d413424b36a2006efd631661790fd77b332ec60
Francois Harbec <fharbec@gmail.com>
2012-11-11 14:31:15 -0800

    update custom settings

--- custom.el
+++ custom.el
@@ -9,8 +9,8 @@
  '(background-color "#042028")
  '(background-mode dark)
  '(cursor-color "#708183")
- '(custom-enabled-themes (quote (pastels-on-dark)))
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "bbebe9c8b15264adf0eb36bfd1d864b5ad8fdd679ea0d7988c5c6de591e15d12" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" default)))
+ '(custom-enabled-themes (quote (railscasts)))
+ '(custom-safe-themes (quote ("24a99ef4aa75121378ecc9db60438d287064db8fb2c6f226fe53f23d31e01539" "047c740e750658d1ac6e7ff72beff516d3cbc9e8ca50ee06415fb6956b551490" "726ae1a6ce9f848dc906937a28a0799f5e8b9ec436b01cbdc3b81bcc55c58263" "4f354b028fa6f8c89881a510bb58ae635d63691d3a65abf098d576e76155ee5d" "6b3ad21446c6b3d0a13c1f34d017a30cccd314ad08a9e1e3b8b1c8254ed7aec4" "7ba08a6fc644e4a45416c2a322b8b31f4c464a0cd3a5169c653664804d20eb44" "9afecd24a05e671c9aae1da3fea6cdbbeb0bffc674137e4cb263ae7bec09f15a" "bcbcb70266c9c5b15276cab4ec974b84a480ef1e8b7c751648b2fdd9c31fcb69" "1dd3c7c877330c45317a4a02bc95a89629903a8797ba91ec7efc8618ef00053a" "7f0ecc2d331b3e6d206efdec32aea270354e47a5d06b54181557ffb50d894743" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "bbebe9c8b15264adf0eb36bfd1d864b5ad8fdd679ea0d7988c5c6de591e15d12" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" default)))
  '(fci-rule-color "#383838")
  '(foreground-color "#708183")
  '(grep-find-ignored-directories (quote (".git" "log" "tmp")))
@@ -20,4 +20,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t nil))))
