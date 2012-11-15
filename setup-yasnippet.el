c20308effd3e2d6ee8b85b8de9491258b89f3184
Francois Harbec <fharbec@gmail.com>
2012-11-14 23:35:34 -0800

    update yasnippet setup

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -1,10 +1,16 @@
 (require 'yasnippet)
-(yas/global-mode 1)
+
 
 ;; Develop and keep personal snippets under ~/emacs.d/mysnippets
-(setq yas/root-directory "~/.emacs.d/elpa/yasnippet-0.6.1/snippets/")
+;;(setq yas/root-directory "~/.emacs.d/elpa/yasnippet-0.6.1/snippets/")
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"            ;; personal snippets
+        "~/.emacs.d/elpa/yasnippet-0.8.0/snippets/" ;; the default collection
+        ))
+
+(yas/global-mode 1)
 
 ;; Load the snippets
-(yas/load-directory yas/root-directory)
+;;(yas/load-directory yas/root-directory)
 
 (provide 'setup-yasnippet)
