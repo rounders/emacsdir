6ef55ecd812b35dd5df16d192cb655c4c3fc308a
Francois Harbec <fharbec@gmail.com>
2012-12-04 23:29:04 -0800

    add a formatter for xml

--- defuns/formatters.el
+++ defuns/formatters.el
@@ -8,3 +8,19 @@
         (e (if mark-active (max (point) (mark)) (point-max))))
     (shell-command-on-region b e
      "python -mjson.tool" (current-buffer) t)))
+
+(defun format-xml()
+  "make xml pretty"
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+     "xmllint --format -" (current-buffer) t)))
+
+;; (defun ruby-pretty-print()
+;;   "use ruby pretty print"
+;;   (interactive)
+;;   (let ((b (if mark-active (min (point) (mark)) (point-min)))
+;;         (e (if mark-active (max (point) (mark)) (point-max))))
+;;     (shell-command-on-region b e
+;;      "ruby -e 'require \"pp\"; pp(eval(ARGF.read));' -" (current-buffer) t)))
