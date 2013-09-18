07b3afffa1b642b3995bdb4240991edf6f0e1d8a
Francois Harbec <fharbec@gmail.com>
2013-09-18 11:15:32 -0700

    apply rhtml mode to .rhtml and .eco files

--- setup-rhtml-mode.el
+++ setup-rhtml-mode.el
@@ -1,6 +1,8 @@
 ;;; rhtml mode
 (add-to-list 'load-path "~/.emacs.d/rhtml")
   (require 'rhtml-mode)
-  (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.eco$" . rhtml-mode))
 
 (provide 'setup-rhtml-mode)
