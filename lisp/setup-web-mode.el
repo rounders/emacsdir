ed0aa0e5c7406de6725b02dbf1741b798583cdf9
Francois Harbec <fharbec@gmail.com>
2016-04-21 12:10:38 -0700

    fix indentation of js in web-mode

--- lisp/setup-web-mode.el
+++ lisp/setup-web-mode.el
@@ -6,7 +6,8 @@
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
